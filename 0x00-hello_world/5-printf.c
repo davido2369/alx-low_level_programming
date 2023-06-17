3268e88ab0549cdbb4f24fe5b7f4e8e6a145a06c
davido2369 <davidnsche@gmail.com>
2023-06-17 09:27:21 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * * main - A program that is using the print f to print a line
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
