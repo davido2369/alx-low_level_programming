ceeb8162eb7f93ddba282c7f115442b541c5fc49
davido2369 <davidnsche@gmail.com>
2023-06-23 08:50:03 -0700

    0x03. C - Debugging

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,9 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
 
-int _putchar(putchar);
+int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x10(void);
+void print_alphabet_xi10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -15,4 +15,3 @@ int add(int, int);
 void print_to_98(int n);
 
 #endif
-
